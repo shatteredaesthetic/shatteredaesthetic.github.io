f881b467fee90343cd71e6d9430bcf2e4c103139
Jason Polhemus <shatteredaesthetic@gmail.com>
2018-05-02 22:42:11 -0400

    initial commit

---
+++ src/components/Bio.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import profilePic from './profile-pic.jpg'
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <img
+          src={profilePic}
+          alt={`Kyle Mathews`}
+          style={{
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            width: rhythm(2),
+            height: rhythm(2),
+          }}
+        />
+        <p>
+          Written by <strong>Jason Polhemus</strong> who lives and works in
+          Atlanta building useful things and making beautiful sound, and hopes
+          to relocate to Chicago soon.{' '}
+          <a href="https://twitter.com/digitalsthtcs">tweets</a>
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Bio
